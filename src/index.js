9cf567b21adc7d1b7d2d3728423c285a9a693fad
mirza <36728610+abusaeedmirza@users.noreply.github.com>
2023-06-08 07:50:27 +0000

    Update 11 files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 var button1=document.getElementById("add-data");
 button1.addEventListener("click", myfunciton);
 function myfunciton(){
-    document.getElementById("lielement").innerHTML="element1";
+    var field=document.getElementById("userinput").value;
+    var ul=document.getElementById("list");
+    var li=document.createElement("li");
+    li.appendChild(document.createTextNode(field));
+    ul.appendChild(li);
 }
